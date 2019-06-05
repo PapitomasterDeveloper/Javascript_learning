14104946ed92c6a621b1d40ebed9990328c4783e
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-05 15:58:21 -0500

    Example consuming the Pokemon API in a class based approach

---
+++ example_20.js
@@ -0,0 +1,15 @@
+class Pokemon {
+	constructor(id) {
+		this.id = id;
+	}
+
+	async logName() {
+		let url = `https://pokeapi.co/api/v2/pokemon/${this.id}`;
+		let response = await $.getJSON(url);
+		console.log(response.name);
+	}
+}
+
+let pokemon = new Pokemon(1);
+
+console.log(pokemon.logName());
